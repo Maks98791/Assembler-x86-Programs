bf3cc34d970bfe564e80daf1d3e6e5dc90ebe276
Maks <Maks98791@gmail.com>
2020-05-04 20:27:17 +0200

    fix functino program

--- programy laby/read_write_exit_func.s
+++ programy laby/read_write_exit_func.s
@@ -17,21 +17,21 @@ msg_echo_len = . -msg_echo
 _start:
 
 # piszemy msg (Wprowadz napis: )
-pushl msg_len # push second argument
-pushl msg # push first argument
+pushl $msg_len # push second argument
+pushl $msg # push first argument
 call write_func
 addl $8, %esp
 int $0x80# wywołanie przerwania programowego -# wykonanie funcji systemowej
 
 # czytamy string do msg_echo
-pushl msg_echo_len # push second argument
-pushl msg_echo # push first argument
+pushl $msg_echo_len # push second argument
+pushl $msg_echo # push first argument
 call read_func
 addl $8, %esp
 int $0x80
 
-pushl msg_echo_len # push second argument
-pushl msg_echo # push first argument
+pushl $msg_echo_len # push second argument
+pushl $msg_echo # push first argument
 call write_func
 addl $8, %esp
 int $0x80
